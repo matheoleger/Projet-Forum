6e8971979c9a156d4a046b2bb5eaa2590f352b14
kevin <kevin.guyodo@ynov.com>
2021-06-01 08:23:36 +0200

    message command line

--- server.go
+++ server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	handlers "./src/handlers"
 )
 
 func main() {
+	const white = "\033[1;37m\033[0m"
+	const blue = "\033[34m"
+	const yellow = "\033[33m"
 
+	fmt.Println(string(blue), "[SERVER_INFO] : Starting local Server...")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))) // récupère tous les fichiers "externe" dans "static"
 
 	http.HandleFunc("/", handlers.Home)
@@ -15,5 +20,7 @@ func main() {
 	http.HandleFunc("/posts", handlers.Posts)
 	http.HandleFunc("/profil", handlers.Profil)
 
+	fmt.Println(string(white), "[SERVER_READY] : on http://localhost:8000 ✅ ")
+	fmt.Println(string(yellow), "[SERVER_INFO] : To stop the program : Ctrl + c")
 	http.ListenAndServe(":8080", nil)
 }
