fce7b539106bc403094a24e2ff6e1619f460fde8
kevin <kevin.guyodo@ynov.com>
2021-06-03 08:18:34 +0200

    Error terminal

--- src/handlers/errors.go
+++ src/handlers/errors.go
@@ -10,14 +10,14 @@ func CodeErreur(w http.ResponseWriter, r *http.Request, status int, message stri
 
 	if status == 404 {
 		http.Error(w, "404 not found", http.StatusNotFound)
-		fmt.Println(message)
+		fmt.Println(string(colorRed), message)
 	}
 	if status == 400 {
 		http.Error(w, "400 Bad request", http.StatusBadRequest)
-		fmt.Println(message)
+		fmt.Println(string(colorRed), message)
 	}
 	if status == 500 {
 		http.Error(w, "500 Internal Server", http.StatusInternalServerError)
-		fmt.Println(message)
+		fmt.Println(string(colorRed), message)
 	}
 }
--- src/handlers/home.go
+++ src/handlers/home.go
@@ -7,14 +7,14 @@ import (
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		CodeErreur(w, r, 404, "Page not found")
+		CodeErreur(w, r, 404, "[Server_Alert] - Error 404 : Page not found")
 	}
 
 	files := findPathFiles("./templates/home.html")
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		CodeErreur(w, r, 500, "Template not found : home.html")
+		CodeErreur(w, r, 500, "[Server_Alert] - Error 500 : Template not found -> home.html")
 	}
 
 	ts.Execute(w, nil)
