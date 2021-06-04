1c8a6b09e048a5554170aae06f5bb58b32b7fee4
Cyriod <71000250+Cyriod@users.noreply.github.com>
2021-06-04 10:38:08 +0200

    Big navbar update
    div -> ul and li
    
    adjusted the active action, made items more versatile and easier to update

--- static/script/navbar.js
+++ static/script/navbar.js
@@ -3,13 +3,13 @@ let flipflop = true;
 function showDiv() {
 
     if (flipflop) {
-        document.getElementById('navbar_menu').style.visibility = "visible";
-        document.querySelector('.navbar_elements').style.visibility = "visible";
+        document.getElementById('navbar_wrapper').style.visibility = "visible";
+        document.querySelector('li').style.visibility = "visible";
         flipflop = false
 
     } else {
-        document.getElementById('navbar_menu').style.visibility = "hidden";
-        document.querySelector('.navbar_elements').style.visibility = "hidden";
+        document.getElementById('navbar_wrapper').style.visibility = "hidden";
+        document.querySelector('li').style.visibility = "hidden";
         flipflop = true
 
 
