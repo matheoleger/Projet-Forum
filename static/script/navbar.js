5257a1e2f61cea9c7ef7b9ecd1959e781ae035b4
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-06-08 09:24:08 +0200

    update navbar (styles and features)
    - added onclick transition navbar
    - added styles (hover) on the link

--- static/script/navbar.js
+++ static/script/navbar.js
@@ -3,15 +3,17 @@ let flipflop = true;
 function showDiv() {
 
     if (flipflop) {
-        document.getElementById('navbar_wrapper').style.visibility = "visible";
-        document.querySelector('li').style.visibility = "visible";
+        // document.getElementById('navbar_wrapper').style.visibility = "visible";
+        // document.querySelector('li').style.visibility = "visible";
+        document.querySelector('.navbar_wrapper').classList.add('navbar_wrapper_appear')
         flipflop = false
+        console.log("coucou")
 
     } else {
-        document.getElementById('navbar_wrapper').style.visibility = "hidden";
-        document.querySelector('li').style.visibility = "hidden";
+        // document.getElementById('navbar_wrapper').style.visibility = "hidden";
+        // document.querySelector('li').style.visibility = "hidden";
+
+        document.querySelector('.navbar_wrapper').classList.remove('navbar_wrapper_appear')
         flipflop = true
-
-
     }
-}
+ }
