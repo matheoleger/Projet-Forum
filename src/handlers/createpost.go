11d618545ecf1045424461abb4ca8dc7b9d1a586
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-06-17 08:53:21 +0200

    base of create posts in database
    - we can create post and put it on the database
    - added styles

--- src/handlers/createpost.go
+++ src/handlers/createpost.go
@@ -4,27 +4,27 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 )
 
-func Creationpost(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/creationpost" {
-		CodeErreur(w, r, 404)
-		return
-	}
+// func Creationpost(w http.ResponseWriter, r *http.Request) {
+// 	if r.URL.Path != "/creationpost" {
+// 		CodeErreur(w, r, 404)
+// 		return
+// 	}
 
-	files := findPathFiles("./templates/createpost.html")
+// 	files := findPathFiles("./templates/createpost.html")
 
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		CodeErreur(w, r, 500)
-		return
-	}
-	ts.Execute(w, nil)
+// 	ts, err := template.ParseFiles(files...)
+// 	if err != nil {
+// 		CodeErreur(w, r, 500)
+// 		return
+// 	}
+// 	ts.Execute(w, nil)
 
-}
+// }
 
-func GetElementOfPost(w http.ResponseWriter, r *http.Request) {
+func CreatePost(w http.ResponseWriter, r *http.Request) {
+
 	err := r.ParseForm()
 	if err != nil {
 		log.Fatal(err)
@@ -34,8 +34,12 @@ func GetElementOfPost(w http.ResponseWriter, r *http.Request) {
 	category := r.PostFormValue("category")
 	content := r.PostFormValue("postcontent")
 
-	fmt.Println("Votre titre est : " + title + " et votre catégorie est :" + category + " puis votre contenu est :" + content)
+	fmt.Println("Votre titre est : " + title + " et votre catégorie est : " + category + " puis votre contenu est : " + content)
+
+	username := "Johanna"
+
+	InsertPost(title, content, username)
 
 	http.Redirect(w, r, "/", http.StatusSeeOther)
-
+	
 }
