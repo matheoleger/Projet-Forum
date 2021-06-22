dc37119612182d3db1ca3c6b690848639c73e566
matheoleger <71370253+matheoleger@users.noreply.github.com>
2021-06-22 14:17:45 +0200

    update login : add alert to specify error
    - added alert to show the problem for the user
    - refactoring to display the right form

--- static/script/login.js
+++ static/script/login.js
@@ -1,16 +1,60 @@
-function showLoginOrRegister(isLoginBtn) {
+// function showLoginOrRegister(isLoginBtn) {
 
-    // let loginBtn = document.getElementById("choice_login_btn");
-    // let registerBtn = document.getElementById("choice_register_btn");
+//     // let loginBtn = document.getElementById("choice_login_btn");
+//     // let registerBtn = document.getElementById("choice_register_btn");
 
-    let loginForm = document.querySelector(".loginForm");
-    let registerForm = document.querySelector(".registerForm");
+//     let loginForm = document.querySelector(".loginForm");
+//     let registerForm = document.querySelector(".registerForm");
 
-    if(isLoginBtn) {
-        loginForm.style.display = "";
-        registerForm.style.display = "none";
-    } else {
-        loginForm.style.display = "none";
-        registerForm.style.display = "";
-    }
+//     if(isLoginBtn) {
+//         loginForm.style.display = "";
+//         registerForm.style.display = "none";
+//     } else {
+//         loginForm.style.display = "none";
+//         registerForm.style.display = "";
+//     }
+
+
+// }
+
+function connexionForm() {
+    window.location.href = "?loginForm=connexion"
+    // showLoginForm()
 }
+
+function inscriptionForm() {
+    window.location.href = "?loginForm=inscription"
+    // showLoginForm()
+}
+
+// function showLoginForm() {
+
+const loginFormElement = document.querySelector(".loginForm");
+const registerForm = document.querySelector(".registerForm");
+
+const queryString = window.location.search
+
+const urlParams = new URLSearchParams(queryString)
+
+const loginForm = urlParams.get("loginForm")
+console.log(loginForm)
+const errType = urlParams.get("err")
+
+if (loginForm == "connexion") {
+    
+    loginFormElement.style.display = "";
+    registerForm.style.display = "none";
+
+} else if (loginForm == "inscription") {
+    
+    loginFormElement.style.display = "none";
+    registerForm.style.display = "";
+
+}
+
+if (errType == "wrong_PW") {
+    alert("Erreur : Vous avez renseigné un mauvais mot de passe...")
+} else if (errType == "wrong_name") {
+    alert("Erreur : Vous avez renseigné un mauvais nom d'utilisateur...")
+}
+// }
