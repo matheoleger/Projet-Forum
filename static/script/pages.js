c356aee8dc42db785b0a2436e25eb8f338f2360c
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2021-06-30 09:11:08 +0200

    updated pagination for all
    updated the pagination functions in pages.js to make it work with the categories and post pages

--- static/script/pages.js
+++ static/script/pages.js
@@ -1,17 +1,27 @@
 function valueFromUrl() {
 	url = window.location.href
+    begin_url = url.match(/^.*(?=(\?))/gm).toString()
+
     post = url.match(/[?&]+post=?([^&]*)?/gi).toString()
     postv = post.match(/\d/gi).toString()
     postvalue = postv.replace(/,/g, '')
+
+    category = url.match(/[?&]+category=?([^&]*)?/gi)
+    if (category != null ) {
+        categoryv = category.toString()
+        categoryvalue = category.replace(/category=/g, '')
+    }
+
     perpage = url.match(/[?&]+perpage=?([^&]*)?/gi).toString()
     perpagev = perpage.match(/\d/gi).toString()
     perpagevalue = perpagev.replace(/,/g, '')
+
     page = url.match(/[?&]+page=?([^&]*)?/gi).toString()
     pagev = page.match(/\d/gi).toString()
     pagevalue = pagev.replace(/,/g, '')
 
     console.log("postvalue=" + postvalue + "perpagevalue=" + perpagevalue + "pagevalue=" + pagevalue )
-    return [postvalue, perpagevalue, pagevalue]
+    return [postvalue, perpagevalue, pagevalue, begin_url]
 }
 
 function nextPage() {
@@ -20,8 +30,19 @@ function nextPage() {
     page = parseInt(values[2]) + 1
 
     console.log("next page" + values)
-    window.location.replace("/posts/content?post=" + values[0] + "&perpage=" + values[1] + "&page=" + page)
 
+    if (values[3] = "http://localhost:8080/posts/content") {
+        window.location.replace("/posts/content?post=" + values[0] + "&perpage=" + values[1] + "&page=" + page)
+    
+    } else if (values[3] = "http://localhost:8080/posts") {
+        window.location.replace("/posts?category=Autres&perpage=" + values[1] + "&page=" + page)
+
+    } else if (values[3] = "http://localhost:8080/categories") {
+        window.location.replace("/categories?perpage=" + values[1] + "&page=" + page)
+
+    } else {
+        window.location.replace("/404")
+    }
 
     // le good regex = /[?&]+page=?([^&]*)?/gi
 }
